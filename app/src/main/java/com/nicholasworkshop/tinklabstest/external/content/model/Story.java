1ca4e03f94e7b1516a031ec0178e03c0ae173cd5
Nicholas Wong <nickwph@gmail.com>
2017-05-16 20:20:46 +0900

    Added test utilities and fakers.

--- app/src/main/java/com/nicholasworkshop/tinklabstest/external/content/model/Story.java
+++ app/src/main/java/com/nicholasworkshop/tinklabstest/external/content/model/Story.java
@@ -1,11 +1,13 @@
 package com.nicholasworkshop.tinklabstest.external.content.model;
 
 import lombok.Data;
+import lombok.experimental.Builder;
 
 /**
  * Created by nicholas.wong on 2017/05/15.
  */
 @Data
+@Builder
 public class Story {
 
     private String title;
---
+++ app/src/test/java/com/nicholasworkshop/tinklabstest/TestUtilities.java
@@ -0,0 +1,27 @@
+package com.nicholasworkshop.tinklabstest;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.internal.schedulers.ExecutorScheduler;
+import io.reactivex.plugins.RxJavaPlugins;
+
+/**
+ * Created by nicholas.wong on 2017/05/16.
+ */
+
+public class TestUtilities {
+
+    public static void initRxSchedulers() {
+        Scheduler immediate = new Scheduler() {
+            @Override
+            public Worker createWorker() {
+                return new ExecutorScheduler.ExecutorWorker(Runnable::run);
+            }
+        };
+        RxJavaPlugins.setInitIoSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitComputationSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitNewThreadSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitSingleSchedulerHandler(scheduler -> immediate);
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(scheduler -> immediate);
+    }
+}
