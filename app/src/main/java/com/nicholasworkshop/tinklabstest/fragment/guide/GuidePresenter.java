10c459fc2974fab53dfa794ae3c61cc13186c0b3
Nicholas Wong <nickwph@gmail.com>
2017-05-17 21:31:08 +0900

    Show toast when data fetching failed.

--- app/src/main/java/com/nicholasworkshop/tinklabstest/fragment/guide/GuidePresenter.java
+++ app/src/main/java/com/nicholasworkshop/tinklabstest/fragment/guide/GuidePresenter.java
@@ -1,10 +1,12 @@
 package com.nicholasworkshop.tinklabstest.fragment.guide;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import javax.inject.Inject;
 
@@ -17,12 +19,14 @@ class GuidePresenter {
     private static final int INITIAL_STORY_COUNT = 4;
     private static final int FETCH_STORY_COUNT = 10;
 
+    private final Context context;
     private final GuideView guideView;
     private final GuideModel guideModel;
     private boolean storyFetching = true;
 
     @Inject
-    GuidePresenter(GuideView guideView, GuideModel guideModel) {
+    GuidePresenter(Context context, GuideView guideView, GuideModel guideModel) {
+        this.context = context;
         this.guideView = guideView;
         this.guideModel = guideModel;
     }
@@ -40,16 +44,20 @@ class GuidePresenter {
         guideModel
                 .storyList(INITIAL_STORY_COUNT)
                 .doOnNext(stories -> storyFetching = false)
-                .subscribe(guideView::setStoryList);
+                .subscribe(guideView::setStoryList, this::handleException);
         guideView
                 .storyRequests()
                 .filter(integer -> !storyFetching)
                 .doOnNext(stories -> storyFetching = true)
                 .flatMap(lastPosition -> guideModel.storyList(lastPosition + FETCH_STORY_COUNT))
                 .doOnNext(stories -> storyFetching = false)
-                .subscribe(guideView::setStoryList);
+                .subscribe(guideView::setStoryList, this::handleException);
         guideModel
                 .ads()
-                .subscribe(guideView::setAdList);
+                .subscribe(guideView::setAdList, this::handleException);
+    }
+
+    private void handleException(Throwable error) {
+        Toast.makeText(context, "Failed to fetch data\n" + error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
     }
 }
